f7ff451298cf5aa466e8e6e29a2e2cceab754e6b
Graham Fairweather <xotic750@gmail.com>
2019-08-15 20:41:38 +0200

    Maintenance

--- dist/to-safe-integer-x.js
+++ dist/to-safe-integer-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2019-present",
-  "date": "2019-08-15T18:01:42.528Z",
+  "date": "2019-08-15T18:41:32.315Z",
   "describe": "",
   "description": "ToSafeInteger converts 'argument' to an integral numeric value of safe limit.",
   "file": "to-safe-integer-x.js",
